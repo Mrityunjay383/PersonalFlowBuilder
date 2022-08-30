39f8294e179fcaca6a2a959df90205d33c43c1e9
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2022-08-31 01:52:55 +0530

    initial template to work made to start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React, {  useState } from "react";
+import { useRete } from "./rete.js";
+
+import "./App.css";
+
+function Editor() {
+  const [setContainer] = useRete();
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh"
+      }}
+      ref={(ref) => ref && setContainer(ref)}
+    />
+  );
+}
 
 function App() {
+  const [visible, setVisible] = useState(true);
+
   return (
     <div className="App">
-
+      {visible && <Editor />}
     </div>
   );
 }
